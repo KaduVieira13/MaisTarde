af8df08e589c3495f6ebc40717323a8f062c0924
KaduVieira13 <carloseduardo_alencar@outlook.com>
2021-04-01 09:15:12 -0300

    Alteração de variável

--- Program.cs
+++ Program.cs
@@ -6,20 +6,25 @@ namespace MaisTarde
     {
         static void Main(string[] args)
         {
-            DateTime atual = DateTime.Now;
+             DateTime atual = DateTime.Now;
             TimeSpan seishtrinta = new TimeSpan(6,30,0);
             DateTime horaAdiantada = atual + seishtrinta;
 
-            Console.WriteLine("--- Exibindo intervalo de 6H30m ---");
+
+            Console.WriteLine("--- Exibindo intervalo de 6H30m --- \n");
             Console.WriteLine($"Hora do sistema: {atual}");
             Console.WriteLine($"Mais tarde: {horaAdiantada}");
 
             string horaDigitada;
 
+
             Console.WriteLine("Digite a hora no formato H,M,s");
             horaDigitada = Console.ReadLine();
-            TimeSpan.Parse(horaDigitada);
-            TimeSpan horadepois =  TimeSpan.Parse (horaDigitada);
+
+
+            int[] conversao = Array.ConvertAll(horaDigitada.Split(','), Convert.ToInt32);
+            TimeSpan horadepois = new TimeSpan(conversao[0],conversao[1],conversao[2]); 
+            
             DateTime horaNova = atual + horadepois;
 
             Console.WriteLine($"Hora do sistema: {atual}");
